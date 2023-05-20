1a40522b04993670707c3e80aa17f5132b7c7b79
TL <TL@gmail.com>
2023-05-20 17:47:26 +0530

    test.java modified by tl

--- Demo/src/com/nt/test/test.java
+++ Demo/src/com/nt/test/test.java
@@ -3,7 +3,7 @@ package com.nt.test;
 import com.nt.beans.A;
 
 public class test {
-public void main(String[]args) {
+public static void main(String[]args) {
 	A a=new A();
 	System.out.println("system date:"+a.getDate());
 }

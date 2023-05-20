64382e5c2dccad3f13505d9bf80ee4f33623276e
TL <TL@gmail.com>
2023-05-20 17:56:11 +0530

    shfsdkljjds

--- Demo/src/com/nt/test/test.java
+++ Demo/src/com/nt/test/test.java
@@ -5,9 +5,9 @@ import com.nt.beans.A;
 public class test {
 public static void main(String[]args) {
 	A a=new A();
-
+	
 	System.out.println("system date:::::"+a.getDate());
-
+System.out.println("hello world");
 }
 	
 }
